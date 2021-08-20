ed6c71d9682a9893dcebe8e8717b5e080621d065
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-08-20 22:17:27 +0800

    Update

---
+++ js/analytics.js
@@ -0,0 +1,17 @@
+// Global site tag (gtag.js) - Google Analytics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-0J2C5XJY4Q');
+// End Google Analytics
+
+// 百度统计
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?aa81d688104ca1dd117a80f7ed7f3883";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})();
+// End 百度统计
