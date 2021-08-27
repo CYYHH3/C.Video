06be60de0ef6ad7198d6f1060693b1d2fcfe5881
CYYHH3 <75060765+CYYHH3@users.noreply.github.com>
2021-08-27 15:55:48 +0800

    Update

--- js/analytics.js
+++ js/analytics.js
@@ -1,4 +1,12 @@
 // Global site tag (gtag.js) - Google Analytics
+var ga = ga || [];
+(function() {
+    var gajs = document.createElement("script");
+    gajs.src = "https://www.googletagmanager.com/gtag/js?id=G-0J2C5XJY4Q";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(gajs, s);
+})();
+
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
@@ -15,3 +23,7 @@ var _hmt = _hmt || [];
     s.parentNode.insertBefore(hm, s);
 })();
 // End 百度统计
+
+// 「我要啦」网站流量统计
+!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"JQ7yKpiv5AQbuI4W",ck:"JQ7yKpiv5AQbuI4W"});
+// End 「我要啦」网站流量统计
